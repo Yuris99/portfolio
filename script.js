b9e67ba708edd2ae435dcad4f3c46f9ee658978c
Yuris99 <mose1207@gmail.com>
2024-06-15 00:37:37 +0900

    add css mode

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function setLanguage(lang) {
     });
 }
 
+function toggleTheme() {
+    const themeStyle = document.getElementById('theme-style');
+    if (themeStyle.getAttribute('href') === 'dark-mode.css') {
+        themeStyle.setAttribute('href', 'light-mode.css');
+    } else {
+        themeStyle.setAttribute('href', 'dark-mode.css');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // 기본 언어 설정
     setLanguage('en');
